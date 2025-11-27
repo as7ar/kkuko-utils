e1582886484487b21434c720195cd9a71b70583b
JUNG TAEWON <153927840+hafskjfha@users.noreply.github.com>
2025-11-27 06:09:50 +0000

    chore: 메타데이터 수정

--- app/word/search/page.tsx
+++ app/word/search/page.tsx
@@ -4,10 +4,11 @@ import WordSearch from './WordSearch';
 export async function generateMetadata() {
     return {
         title: "끄코 유틸리티 - 오픈DB 단어검색",
-        description: `끄코 유틸리티 - 오픈DB 단어 검색 홈`,
+        description: `끄투코리아 오픈DB 단어 검색`,
+        keywords: ["끄투코리아", "단어검색", "검색기", "끄투코리아 단어", "끄코 검색기"],
         openGraph: {
             title: "끄코 유틸리티 - 오픈DB 단어검색",
-            description: "끄코 유틸리티 - 오픈DB 단어 검색 홈",
+            description: "끄투코리아 오픈DB 단어 검색",
             type: "website",
             url: "https://kkuko-utils.vercel.app/word/search",
             siteName: "끄코 유틸리티",
